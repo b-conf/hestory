028e57904944afc72f5540fcf865c26480dc2ac3
龙腾道 <LongTengDao@LongTengDao.com>
2021-08-11 18:16:38 +0800

    Create toml.js, Update package.json, Update toml.js, Create 016-template-stringify.toml

---
+++ adaptors/toml.js
@@ -0,0 +1,11 @@
+
+import { parse } from '@ltd/j-toml';
+
+const BOM = '\uFEFF';
+
+export function parseTOML (source) {
+  if ( typeof source==='string' && source.startsWith(BOM) ) {
+    source = source.slice(1);
+  }
+  return parse(source, 1.0, '\n');
+}
